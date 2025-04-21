40a45bfa02e519b50ce9014f0ac5cedcb4367c42
Fatema Alsaaidi <116080014+FatemaAlsaaidi@users.noreply.github.com>
2025-04-21 15:56:34 +0400

    start "Reverse a String Using Stack'

--- StakQueueFilesTasks/StakQueueFilesTasks/Program.cs
+++ StakQueueFilesTasks/StakQueueFilesTasks/Program.cs
@@ -4,8 +4,31 @@
     {
         static void Main(string[] args)
         {
-            // ************************************* Stack *****************************************
-            // =====================================  Easy =========================================
+
+            Console.WriteLine("Enter the number of task :");
+            Console.WriteLine("1. Reverse a String Using Stack");
+
+            int Choice = int.Parse(Console.ReadLine());
+            switch (Choice)
+            {
+                case 1:
+                    Console.WriteLine("Enter any word :");
+                    string InputWord = Console.ReadLine();
+                    ReverseAString(InputWord);
+                    Console.ReadLine();
+                    break;
+            }
+
+        }
+
+        // ************************************* Stack *****************************************
+        // =====================================  Easy =========================================
+        //1. Reverse a String Using Stack 
+        public static string ReverseAString(string word)
+        {
+            
+            
+
 
         }
     }
