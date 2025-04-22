13edcfd18ba9fe38a0f45b322c9c5ae2d9fdea9b
Fatema Alsaaidi <116080014+FatemaAlsaaidi@users.noreply.github.com>
2025-04-22 16:54:53 +0400

    start code "Evaluate Postfix Expression "

--- StakQueueFilesTasks/StakQueueFilesTasks/Program.cs
+++ StakQueueFilesTasks/StakQueueFilesTasks/Program.cs
@@ -1,12 +1,16 @@
-﻿namespace StakQueueFilesTasks
+﻿using System.Diagnostics.Metrics;
+
+namespace StakQueueFilesTasks
 {
     internal class Program
     {
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Enter the number of task :");
+            Console.WriteLine("Stak Queue Files Tasks");
             Console.WriteLine("1. Reverse a String Using Stack");
+            Console.WriteLine("2. Evaluate Postfix Expression");
+            Console.WriteLine("Enter the number of task :");
 
             int Choice = int.Parse(Console.ReadLine());
             switch (Choice)
@@ -15,6 +19,12 @@
                     Console.WriteLine("Enter any word :");
                     string InputWord = Console.ReadLine();
                     ReverseAString(InputWord);
+                    Console.ReadLine();
+                    break;
+                case 2:
+                    Console.WriteLine("the formula");
+                    string formula = Console.ReadLine();
+                    EvaluatePostfixExpression(formula);
                     Console.ReadLine();
                     break;
             }
@@ -36,9 +46,55 @@
             {
                 Console.Write(let2);
             }
+        }
+        //2. Evaluate Postfix Expression 
+        public static double EvaluatePostfixExpression(string formula)
+        {
+            bool isdigit = true;
+            int num;
+            double res;
+            char op;
+            Stack<char> bit = new Stack<char>(); // Stack of letter 
+            foreach (char a in formula)
+            {
+                bit.Push(a);
+            }
+
+            foreach (char a in bit)
+            {
+                
+                char Top_a = bit.Pop();
+
+                if (Char.IsDigit(Top_a))
+                {
+                    isdigit = true;
+                }
+                else
+                {
+                    isdigit = false;
+                }
+                if (isdigit)
+                {
+                    num= (int)char.GetNumericValue(Top_a);
+                }
+                else
+                {
+                    op = Top_a;
+                    switch (op)
+                    {
+                        case '*':
+                            num = 1;
+                            res = num * num;
+                            break;
+                        case '-':
+
+                            res = res - num;
+                            break;
+                    }
+                }
+            }
+
             
-
-
         }
     }
 }
